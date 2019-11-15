4b36eed7da19bdf6f603e3530104b29bf6ceadd9
carlosnsilva <cmanoel17@gmail.com>
2019-11-15 16:09:20 -0300

    Continuação do APP

--- ranking/admin.py
+++ ranking/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import cadastro
 # Register your models here.
+
+admin.site.register(cadastro)
--- ranking/models.py
+++ ranking/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 
-class cadastro(models.Models):
+class cadastro(models.Model):
 
     # Nome
-        nome = models.CharField(max_lenght = 200)
+        nome = models.CharField(max_length = 200)
 
     # Campus
         campus_choice = (
@@ -20,7 +20,23 @@ class cadastro(models.Models):
             ('IFPB - ITABAIANA', 'IFPB - CAMPUS: ITABAIANA'),
             ('IFPB - ITAPORANGA', 'IFPB - CAMPUS: ITAPORANGA'),
             ('IFPB - JOÃO PESSOA', 'IFPB - CAMPUS: JOÃO PESSOA'),
-        )
+            ('IFPB - JOÃO PESSOA', 'IFPB - CAMPUS: MANGABEIRA'),
+            ('IFPB - MONTEIRO', 'IFPB - CAMPUS: MONTEIRO'),
+            ('IFPB - PATOS', 'IFPB - CAMPUS: PATOS'),
+            ('IFPB - PEDRAS DE FOGO', 'IFPB - CAMPUS: PEDRAS DE FOGO'),
+            ('IFPB - PICUÍ', 'IFPB - CAMPUS: PICUÍ'),
+            ('IFPB - PRINCESA ISABEL', 'IFPB - CAMPUS: PRINCESA ISABEL'),
+            ('IFPB - SANTA LUZIA', 'IFPB - CAMPUS: SANTA LUZIA'),
+            ('IFPB - SANTA RITA', 'IFPB - CAMPUS: SANTA RITA'),
+            ('IFPB - SOLEDADE', 'IFPB - CAMPUS: SOLEDADE'),
+            ('IFPB - SOUSA', 'IFPB - CAMPUS: SOUSA'),
+            )
+
+
+        campus = models.CharField(
+            max_length = 20,
+            choices = campus_choice
+            )
 
     # Tempo
         tempo = models.CharField(max_length = 10)
@@ -30,3 +46,7 @@ class cadastro(models.Models):
     
     # Penalidades
         penalidade = models.CharField(max_length = 500)
+
+
+def __str__(self):
+    return self.nome

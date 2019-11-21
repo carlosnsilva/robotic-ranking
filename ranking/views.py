bd31ccb270a8b5d458f715478e23cc7ec2af1d4f
carlosnsilva <cmanoel17@gmail.com>
2019-11-21 15:21:53 -0300

    Término do Calculo das Penalidades

--- ranking/views.py
+++ ranking/views.py
@@ -71,4 +71,34 @@ def Calculando(request,id):
         (value.Valor_da_penalidade_11)/100
     ]
    
+   # Separando o minuto do segundo, 
+   # Convertendo os minutos em segundos e somando os minutos convertidos com os segundos
 
+    minuto = tempo // 1
+    segundos = tempo % 1
+    m_segundo = (minuto * 60)
+    tempo_total = m_segundo + segundos
+
+    # Aplicando as porcentagens das penalidades
+
+    p = sum(array_penal)
+
+    penalidade = (tempo_total * p)
+
+    tempo_penalidade = (tempo_total + penalidade) # OBS -> o tempo penalidade está todo em segundo
+
+    tempo_total_min = (tempo_penalidade/60) # OBS -> o tempo penalidade está todo em minutos, separar agr o minuto do segundo para ficar legível
+
+    minuto_total = (tempo_total_min // 1) # Separando os minutos após aplicar as penalidades
+    
+    segundo_total_min = (tempo_total_min % 1) # Separando os segundos após aplicar as penalidades
+                                            # Aqui os segundos estão em minutos, precisa converter para segundo de forma legível
+
+    segundo_total_seg = (segundo_total_min * 60) # Transformando os segundos que estão em minutos em segundos corretamente
+
+    tempo_result = (minuto_total + segundo_total_seg) #   OBS -> tempo em forma legível
+
+    # Colocando no Banco o novo valor do tempo após aplicado as penalidades
+    value.tempo = tempo_result
+
+    return redirect('read')
